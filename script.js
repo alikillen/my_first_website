01e1dc425b024f2553691f8016580f2bebe94629
Alison Killen <61318985+alikillen@users.noreply.github.com>
2025-09-20 13:07:22 +1000

    js stuff

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('hello word');
+console.log('hello world');
+
+// this is some javascript code from
+
+
+function toggleMenu() {
+  var mobileNav = document.querySelector(".mobile-nav");
+
+  if (mobileNav.style.display === "block") {
+    mobileNav.style.display = "none";
+  } else {
+    mobileNav.style.display = "block";
+  }
+  console.log("toggle!");
+}
